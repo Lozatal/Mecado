490c28692ead901d30ca8a3846df87d2a355bb8e
daniel <db.daniel.bentz@gmail.com>
2017-11-07 08:57:43 +0100

    commentaire
    Ajout des commentaires du main

--- main.php
+++ main.php
@@ -1,13 +1,17 @@
 <?php
 
+	//Démarrage de la session utilisateur
 	session_start();
+	
+	//liste des requires
 	require_once('vendor/autoload.php');
-
-
 	require_once 'src/mf/utils/ClassLoader.php';
+	
+	//On initie le classLoader
 	$loader = new mf\utils\ClassLoader('src');
 	$loader->register(); 
 
+	//connexion à la base de donnée
 	$config = parse_ini_file('conf/config.ini');
 
 	$db = new Illuminate\Database\Capsule\Manager();
@@ -15,7 +19,8 @@
 	$db->addConnection($config);
 	$db->setAsGlobal();
 	$db->bootEloquent();
-
+	
+	//On va définir la liste des routes
 	$router = new mf\router\Router();
 
 	$router->addRoute('home',    '/home/',         '\mecadoapp\control\MecadoController', 'viewHome', \mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_NONE);
@@ -27,7 +32,7 @@
 	$router->addRoute('listes',    '/listes/',         '\mecadoapp\control\ListeController', 'listes', \mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 	$router->addRoute('check_liste',    '/check_liste/',         '\mecadoapp\control\ListeController', 'chekListe', \mecadoapp\auth\MecadoAuthentification::ACCESS_LEVEL_USER);
 
-
+	//route par défault si jamais l'url est fausse
 	$router->addRoute('default', 'DEFAULT_ROUTE',  '\mecadoapp\control\MecadoController', 'viewHome');
 
 	$router->run();
